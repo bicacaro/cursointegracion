d1e59e64872f21c126422169d37c0f71ecc18feb
Tomas González <tgonbarl@gobiernodecanarias.org>
2021-05-27 08:38:55 +0000

    corrección errores

--- src/main/java/es/gobcan/ece/integracion1/Menu.java
+++ src/main/java/es/gobcan/ece/integracion1/Menu.java
@@ -11,9 +11,9 @@ public class Menu {
 		Operaciones.multiplicacion(elem1, elem2);
 		Operaciones.division(elem1, elem2);
 		System.out.println("La suma es " + Prueba19052021.suma(elem1, elem2));
-		System.out.println("La resta es " + Operaciones.resta(elem1, elem2););
+		System.out.println("La resta es " + Operaciones.resta(elem1, elem2));
 		System.out.println("La multiplicación es " + Operaciones.multiplicacion(elem1, elem2));
-		System.out.println("La división es " + Operaciones.division(elem1, elem2);
+		System.out.println("La división es " + Operaciones.division(elem1, elem2));
 	}
 
 }
